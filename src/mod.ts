3590f098baac9e3267d9d5bf9e346fa26c19d614
denyncrawford <kiquerangelb@gmail.com>
2022-05-30 02:55:52 -0300

    feat: env vars

--- src/mod.ts
+++ src/mod.ts
@@ -2,20 +2,26 @@ import express, { Request, Response } from "express";
 import { RoomStream } from "./room_stream";
 import { initIoListener } from "./io";
 import cors from "cors";
+import { config } from "dotenv";
 
+config();
 const app = express();
+const port = process.env.PORT || 8081;
 
 app.use(cors());
 
-const server = app.listen(8081, () => {
+const server = app.listen(port, () => {
   console.log("http://localhost:8081");
 });
 
 const activeRooms = new Map<string, RoomStream>();
-
 initIoListener(server, activeRooms);
 
 // Stream route
+
+// Todo create register stream route
+// Create metadata route for the stream /stream/:id/metadata
+// create index route for available streams /streams
 
 app.get("/stream/:id", (req: Request, res: Response) => {
   const { id } = req.params;
